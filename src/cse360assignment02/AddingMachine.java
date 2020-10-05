de5a3dc3b670dd767b1e01560d4f187eb001ff9c
cnjarami <cnjarami@asu.edu>
2020-10-05 17:23:37 -0600

    initial AddingMachine commit

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,44 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
+    private String print;
 
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
     }
 
     public int getTotal () {
-        return 0;
+        return total;
     }
 
     public void add (int value) {
+        if(print == null){
+            print = total + " + " + value;
+        }else{
+            print += " + " + value;
+        }
+        total = total + value;
     }
 
     public void subtract (int value) {
+        if(print == null){
+            print = total + " - " + value;
+        }else{
+            print += " - " + value;
+        }
+        total = total - value;
     }
 
     public String toString () {
-        return "";
+        if(print == null){
+            return "0";
+        }else {
+            return print;
+        }
     }
 
     public void clear() {
+        print = null;
+        total = 0;
     }
 }
